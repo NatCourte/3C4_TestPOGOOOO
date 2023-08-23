c85113070384e16243ed8549991827cb8bed552a
Nathaniel Courtemanche <6194275@cstj.qc.ca>
2023-08-23 17:26:11 -0400

    Added buttons and messages

--- MASOLUTIONPOGO/MASOLUTIONPOGO/App.xaml.cs
+++ MASOLUTIONPOGO/MASOLUTIONPOGO/App.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+
+
 namespace MASOLUTIONPOGO
 {
     /// <summary>
@@ -13,5 +15,30 @@ namespace MASOLUTIONPOGO
     /// </summary>
     public partial class App : Application
     {
+        public List<int> asd = new List<int>()
+        {
+            1,
+            2,
+            3,
+        };
+
+
+
+        public static List<Doctor> Doctors = new List<Doctor>()
+        {
+            new Doctor() { Id = "456" },
+            new Doctor(),
+            new Doctor(),
+        };
+    }
+
+
+
+    public class Doctor
+    {
+        public string Id { get; init; }
+        public string Name { get; set; }
     }
 }
+
+
--- MASOLUTIONPOGO/MASOLUTIONPOGO/MainWindow.xaml.cs
+++ MASOLUTIONPOGO/MASOLUTIONPOGO/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace MASOLUTIONPOGO
     /// </summary>
     public partial class MainWindow : Window
     {
+        //public Doctor doc123 = new Doctor() { Id = "123" };
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            ButtonClickMe.Click += Button_ClickFirstTime;
+
+            TextBlockGame.Text = "Baldurs Gate 3";
+
+            TextBlock newTextBlock = new TextBlock();
+            newTextBlock.Text = "This is Crazy!";
+
+            StackPanelMain.Children.Add(newTextBlock);
+        }
+
+        private void Button_ClickFirstTime(object sender, RoutedEventArgs e)
+        {
+            //doc123.Name = "poutine";
+
+            //List<Doctor> doctors = App.Doctors;
+            foreach (var doctor in App.Doctors)
+            {
+
+            }
+
+            string name = TextBoxName.Text;
+            MessageBox.Show($"Bonjour {name}!", "Title",MessageBoxButton.OK, MessageBoxImage.Warning);
+            //MessageBox.Show("Text message patate!", "Title", MessageBoxButton.OK, MessageBoxImage.Error);
+            ButtonClickMe.Click -= Button_ClickFirstTime;
+            ButtonClickMe.Click += Button_ClickSecondTime;
+
+        }
+
+        private void Button_ClickSecondTime(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("YOU WIN!", "Title", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
